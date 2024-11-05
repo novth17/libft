b5b1366ba64564040d9d9f30fe0e0297aca899e6
Hien Nguyen <hien.nguyen2@myy.haaga-helia.fi>
2024-11-05 22:09:37 +0200

    try to do ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -35,23 +35,71 @@ static int count_words(const char *s, char c)
 	return (count);
 }
 
+static char *copy_word(const char *s, int start, char c)
+{
+	int end;
+	int word_len;
+	int i;
+
+	end = start;
+
+	while (s[end] != '\0' && s[end] != c)
+		end++;
+	word_len = end - start;
+	char *word = malloc((word_len + 1) * sizeof(char));
+	if (word == NULL)
+		return NULL;
+	i = 0;
+	while (i < word_len)
+	{
+		word[i] = s[start + i];
+		i++;
+	}
+	word[word_len] = '\0';
+	return word;
+}
+
 
 char **ft_split(char const *s, char c)
 {	
 	const int	word_count = count_words(s, c);
-	char		**words;
+	char		**all_words;
+	int	start;
+	int index;
 
-	words = malloc((word_count + 1) * sizeof(char *));
-
-	words[0] = "ab";
-	words[1] = "bc";
-	words[2] = NULL;
-
-	printf("%s\n", words[0]);
-	printf("%s\n", words[1]);
-	printf("is NULL? %d\n", words[2] == NULL);
-
+	all_words = malloc((word_count + 1) * sizeof(char *));
+	if (!all_words)
+		return NULL;
+	
+	start = 0;
+	index = 0;
+	while (s[start] != '\0')
+	{
+		//skip delimiters
+		while (s[start] == c)
+			start++;
+		//if end of string, break the loop
+		if (s[start] == '\0')
+			break;
+		//copy each word
+		all_words[index] = copy_word(s, start, c);
+		if (all_words[index] == NULL)
+		{
+			while (index > 0)
+				free(all_words[--index]);
+            free(all_words);
+            return NULL;
+		}
+		//If you just freed all_words without freeing each word first, you'd lose the pointers to those words, which //means they would be inaccessible and the memory would not be freed, leading to a memory leak.
+		index++;
+		//move start to the end
+		while (s[start] != '\0' && s[start] != c)
+			start++;
+	}
+	all_words[index] == '\0';
+	return all_words;
 }
+
 
 int main()
 {
