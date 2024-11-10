955103b7acf2ae49d685140ae6682c9bef962303
Hien Nguyen <hien.nguyen2@myy.haaga-helia.fi>
2024-11-10 20:09:45 +0200

    just draft out ft_lstclear.c that has main inside

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -11,8 +11,44 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-void ft_lstclear(t_list **lst, void (*del)(void*));
+void ft_lstclear(t_list **lst, void (*del)(void*))
 {
+	t_list *temp;
+	if (lst == NULL || *lst == NULL || del == NULL)
+		return ;
 	
+	while (*lst != NULL)
+	{
+		temp = (*lst)->next;
+		del((*lst)->content);
+		free(*lst);
+		*lst = temp;
+	}
+	(*lst) = NULL;
+}
+
+static void del(void *content)
+{
+    free(content);
+}
+
+int main ()
+{
+    // Dynamically allocate content for each node to ensure it can be freed
+    t_list *node1 = ft_lstnew(ft_strdup("15"));
+    t_list *node2 = ft_lstnew(ft_strdup("20"));
+    t_list *node3 = ft_lstnew(ft_strdup("25"));
+
+	node1->next = node2;
+	node2->next = node3;
+	node3->next = NULL;
+
+	ft_lstclear(&node1, del);
+
+	if (node1 == NULL)
+		printf("List cleared!\n");
+
+	return 0;
 }
