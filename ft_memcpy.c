e226b6d04258f289ffd910183373871ae2044a98
Hien Nguyen <hiennguy@c2r1p4.hive.fi>
2024-11-12 17:59:28 +0200

    Fixed crash

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hiennguy <hiennguy@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:11:38 by hiennguy          #+#    #+#             */
-/*   Updated: 2024/11/12 17:56:28 by hiennguy         ###   ########.fr       */
+/*   Updated: 2024/11/12 17:59:19 by hiennguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	const unsigned char	*s;
 	size_t				i;
 
-	// if (!dest || !src)
-	// 	return (dest);
-	if (n == 0)
+	if (!dest && !src)
 		return (dest);
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
