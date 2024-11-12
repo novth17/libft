f64a0c2394725ce2c853786991845f789f7cc859
Hien Nguyen <hiennguy@c2r1p4.hive.fi>
2024-11-12 17:10:56 +0200

    try to fix ft substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hiennguy <hiennguy@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 13:41:01 by hiennguy          #+#    #+#             */
-/*   Updated: 2024/11/12 16:58:46 by hiennguy         ###   ########.fr       */
+/*   Updated: 2024/11/12 17:02:28 by hiennguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	s_len;
+	size_t	substr_length;
 
 	s_len = ft_strlen(s);
 	if (start >= s_len)
 		return (ft_strdup(""));
+	substr_length = len;
 	if (len > s_len - start)
-        len = s_len - start;
-	substr = malloc((s_len + 1) * sizeof(char));
+		substr_length = s_len - start;
+	substr = malloc((substr_length + 1) * sizeof(char));
 	if (substr == NULL)
 		return (NULL);
 	ft_strlcpy (substr, s + start, len + 1);
