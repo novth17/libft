d036a93c089846ed28929f984578bb3ee941c4e3
Hien Nguyen <hiennguy@c2r1p4.hive.fi>
2024-11-12 19:26:05 +0200

    Handled len >= SIZE_MAX

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hiennguy <hiennguy@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 13:41:01 by hiennguy          #+#    #+#             */
-/*   Updated: 2024/11/12 19:19:02 by hiennguy         ###   ########.fr       */
+/*   Updated: 2024/11/12 19:25:30 by hiennguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	s_len;
 	size_t	substr_length;
 
-	if (len >= SIZE_MAX)
-		return (NULL);
 	s_len = ft_strlen(s);
 	if (start >= s_len)
 		return (ft_strdup(""));
 	substr_length = len;
 	if (len > s_len - start)
 		substr_length = s_len - start;
+	if (substr_length >= SIZE_MAX)
+		substr_length = SIZE_MAX - 1;
 	substr = malloc((substr_length + 1) * sizeof(char));
 	if (substr == NULL)
 		return (NULL);
