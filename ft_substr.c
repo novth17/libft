2679d4c4c3c14ae849718857a86b7f28af977206
Hien Nguyen <hiennguy@c2r1p4.hive.fi>
2024-11-12 16:31:38 +0200

    fix s_len malloc part

--- ft_substr.c
+++ ft_substr.c
@@ -6,21 +6,21 @@
 /*   By: hiennguy <hiennguy@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 13:41:01 by hiennguy          #+#    #+#             */
-/*   Updated: 2024/11/03 17:50:52 by hiennguy         ###   ########.fr       */
+/*   Updated: 2024/11/12 16:30:45 by hiennguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
-{	
+{
 	char	*substr;
 	size_t	s_len;
 
 	s_len = ft_strlen(s);
 	if (start >= s_len)
 		return (ft_strdup(""));
-	substr = malloc((len + 1) * sizeof(char));
+	substr = malloc((s_len + 1) * sizeof(char));
 	if (substr == NULL)
 		return (NULL);
 	ft_strlcpy (substr, s + start, len + 1);
