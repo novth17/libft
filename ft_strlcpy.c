b8a8b6f6c0b8433fd2957cb10aa1fbab143b4b2a
Hien Nguyen <hiennguy@c2r1p4.hive.fi>
2024-11-12 17:20:39 +0200

    fix crash

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: hiennguy <hiennguy@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:11:38 by hiennguy          #+#    #+#             */
-/*   Updated: 2024/11/12 17:17:29 by hiennguy         ###   ########.fr       */
+/*   Updated: 2024/11/12 17:20:34 by hiennguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	src_len;
 	size_t	i;
 
-	if (src == NULL)
-		return (0);
 	src_len = ft_strlen(src);
 	if (size == 0)
 		return (src_len);
