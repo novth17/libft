86d427b68a04cc1ff42f15f1544ee0bd04b1912e
Hien Nguyen <hiennguy@c2r1p4.hive.fi>
2024-11-12 16:23:20 +0200

    fixing strcompare

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hiennguy <hiennguy@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 17:11:38 by hiennguy          #+#    #+#             */
-/*   Updated: 2024/11/12 16:21:12 by hiennguy         ###   ########.fr       */
+/*   Updated: 2024/11/12 16:22:57 by hiennguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,21 +30,4 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 		i++;
 	}
 	return (0);
-}
-
-#include <stdio.h>
-#include <string.h>
-#include "libft.h"
-int	ft_memcmp (const void *s1, const void *s2, size_t n);
-
-int main()
-{
-	char test1[] = {0, 0, 127, 0};
-	char test2[] = {0, 0, 42, 0};
-
-	printf("my ft_memcmp is %d\n", ft_memcmp(test1, test2, 4));
-
-	printf("REAL memcmp is %d\n", memcmp(test1, test2, 4));
-
-	return 0;
 }
